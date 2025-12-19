e00b273450ad9a6dbd1902da708596207899dd17
Oleksandr Polotniuk <oleksandr.polotniuk@quarks.tech>
2025-12-19 12:33:27 +0200

    ts builded

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 console.log('Renderer meta framework initialized');
-
 export const printHello = (name = 'World') => {
     console.log(`Hello ${name}`);
-}
-
+};
 export const sum = (a, b) => a + b;
-
 export const subtract = (a, b) => a - b;
